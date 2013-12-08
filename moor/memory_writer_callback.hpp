dcc746580b89e23b1d92bb5f7f9b71770c339a7d
Mohammad Mehdi Saboorian <saboorian@gmail.com>
2013-12-08 11:04:43 +0330

    fixes conflict for ssize_t in  MinGW

--- moor/memory_writer_callback.hpp
+++ moor/memory_writer_callback.hpp
@@ -26,10 +26,12 @@
 
 #include <list>
 
-#if defined (_WIN64)
-  typedef long long ssize_t;
-#elif defined (_WIN32)
-  typedef long ssize_t;
+#if !defined(__MINGW32__) && !defined(__MINGW64__) && !defined(__CYGWIN__)
+#  if defined (_WIN64)
+     typedef long long ssize_t;
+#  elif defined (_WIN32)
+     typedef long ssize_t;
+#  endif
 #endif
 
 struct archive;
